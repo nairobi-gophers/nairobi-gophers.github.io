8d07a9467d8875aca21aab895e66f0b899b35ab7
James Kaguru Kihuha <47499826+James-Kaguru@users.noreply.github.com>
2023-11-09 11:21:24 +0300

    setup tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>We Are <br/> Nairobi Golfers</h1>
+<p>We are a group is for Go Enthusiasts. We aim to create a community
+    around the language and share experiences adopting the language.</p>
