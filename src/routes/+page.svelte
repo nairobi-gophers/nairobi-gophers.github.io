ed2e612f67515770c0c95fc7d7ef455b59a64147
James Kaguru Kihuha <47499826+James-Kaguru@users.noreply.github.com>
2023-11-09 11:39:40 +0300

    added main page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,21 @@
-<h1>We Are <br/> Nairobi Golfers</h1>
+<script>
+    import nairobiGophersLogo from '$lib/assets/images/nairobi-gophers-logo.png'
+</script>
+<nav>
+    <div class="w-11 h-auto">
+        <img src={nairobiGophersLogo} alt="Nairobi gophers logo" />
+    </div>
+    <div>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Nairobigophercon 2024</a>
+    </div>
+</nav>
+
+<h1>We Are <br/> Nairobi Gophers</h1>
 <p>We are a group is for Go Enthusiasts. We aim to create a community
     around the language and share experiences adopting the language.</p>
+
+
+<button type="button">Find us on meetup</button>
